17ca71e4f26faf539e973c745327c195bb9757ef
Rick Waterson <rick@blossomstreet.net>
2024-03-02 19:09:23 -0600

    Added player_select_list to helpers

--- MarksmanshipCentral/app/helpers.py
+++ MarksmanshipCentral/app/helpers.py
@@ -1,5 +1,9 @@
 #Common Functions Go here. reference = from app.helpers import * 
 
+
+
+from django.db.models import Value
+from django.db.models.functions import Concat
 from models.models import Person, Role
 
 
@@ -14,3 +18,10 @@ from models.models import Person, Role
     
     
     
+def player_select_list():
+    plist = Person.objects.all().order_by("lastname","firstname").annotate(full_name = Concat('lastname',Value(', '),'firstname')).values_list("id","full_name")
+    i = plist.__len__
+
+
+def something_good():
+    return 'lemon pie'    
