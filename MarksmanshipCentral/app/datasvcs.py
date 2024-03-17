faab0f6237da2230b11d6a5499e820c36b331d06
ramchip50 <ramchip50@gmail.com>
2024-03-16 21:16:45 -0500

    Added datasvcs to support report generation. Swapped database to mySql.

--- MarksmanshipCentral/app/datasvcs.py
+++ MarksmanshipCentral/app/datasvcs.py
@@ -3,13 +3,17 @@
 
 from django.db import connection
 from collections import namedtuple
+from models.models import *
+from datetime import *
+
 
 def gettotal_sql():
     mbr = 7014
+    wpn = 'Rifle'
     #query = "Select person_id,weapon_id,marksman,sharpshooter,expert,high_expert from models_totalcredits where active = 1 and person_id = %s"
-    query = "Select lastname, firstname,weapon,weaponcredits from modelview_categorycredits where person_id = %s"
+    query = "Select lastname, firstname,weapon,weaponcredits from modelview_categorycredits where person_id = %s and weapon = %s"
     with connection.cursor() as cursor:
-        cursor.execute(query,[mbr])
+        cursor.execute(query,[mbr,wpn])
         results = namedtuplefetchall(cursor)
         
     return results
@@ -22,3 +26,34 @@ def namedtuplefetchall(cursor):
     desc = cursor.description
     nt_result = namedtuple("Result", [col[0] for col in desc])
     return [nt_result(*row) for row in cursor.fetchall()]
+
+def get_allsessioncredits_bydate_andchapter(startdate:datetime,enddate:datetime,chapter:Chapter):
+    allsessions = get_allsessioncredits_bydate(startdate,enddate)
+    result = list()
+    for sess in allsessions:
+        if sess.chapter == chapter.name:
+            result.append(sess)
+      
+    return result
+
+def get_allsessioncredits_bydate_andfleet(startdate:datetime,enddate:datetime,fleet:Fleet):
+    allsessions = get_allsessioncredits_bydate(startdate,enddate)
+    result = list()
+    for sess in allsessions:
+        if sess.fleet == fleet.name:
+            result.append(sess)
+
+    return result
+
+def get_allsessioncredits_bydate(startdate:datetime,enddate:datetime):
+    
+    with connection.cursor() as cursor:
+        cursor.callproc('get_all_sessioncredits_bydate',[startdate,enddate])
+        results = namedtuplefetchall(cursor)
+
+    return results
+
+
+
+    
+    
