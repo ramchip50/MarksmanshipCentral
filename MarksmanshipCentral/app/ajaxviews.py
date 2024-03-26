61f92f9d371560a0e92193e091c7657a96394b26
ramchip50 <ramchip50@gmail.com>
2024-03-25 20:53:07 -0500

    WIP

--- MarksmanshipCentral/app/ajaxviews.py
+++ MarksmanshipCentral/app/ajaxviews.py
@@ -28,4 +28,38 @@ def member_autocomplete(request):
 		return JsonResponse(json, safe=False)
 
 
+def update_scoring(session_id:int,game_id:int):
+	g = Game.active_objects.get(id=game_id)
+	pl = SessionParticipants.active_objects.filter(session_id=session_id)
+	for p in pl:
+		update_total_credits(p.person_id,p.earned_credits,g)
+
+def game_approve(request):
+		g_id = request.GET['game_id']
+		game = Game.active_objects.get(pk=g_id)
+		game.verified = True
+		game.save()
+		sess=Session.active_objects.filter(game_id=g_id)
+		update_scoring(sess.id,g_id)
+		return no_coffee()
+
+def game_replace(request):
+	g_id_new = request.GET['new_game_id']
+	g_id_old = request.GET['old_game_id']
+	new_g=Game.active_objects.get(id=g_id_new)
+	new_g.verified=True
+	new_g.save()
+	sess=Session.active_objects.filter(game_id=g_id_old)
+	for s in sess:
+		s.game_id=g_id_new
+		update_scoring(s.id,g_id_new)
+	old_g=Game.active_objects.get(id=g_id_old)
+	old_g.delete()
+	return no_coffee()
+
+def game_edit(request):
+			
+	
+ 	return no_coffee()
+
 
